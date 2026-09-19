8a19f63d7880e1115e5dbc947b68b64de00452f7
Mahmoud <mahmoudnasser1561@gmail.com>
2026-09-19 16:16:18 +0300

    Add route tables for the public, app and data tiers

---
+++ modules/networking/routes.tf
@@ -0,0 +1,56 @@
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "${var.project}-public"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count = length(aws_subnet.public)
+
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "app" {
+  count = length(local.azs)
+
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
+
+  tags = {
+    Name = "${var.project}-app-${local.az_suffixes[count.index]}"
+  }
+}
+
+resource "aws_route_table_association" "app" {
+  count = length(aws_subnet.app)
+
+  subnet_id      = aws_subnet.app[count.index].id
+  route_table_id = aws_route_table.app[count.index].id
+}
+
+resource "aws_route_table" "data" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.project}-data"
+  }
+}
+
+resource "aws_route_table_association" "data" {
+  count = length(aws_subnet.data)
+
+  subnet_id      = aws_subnet.data[count.index].id
+  route_table_id = aws_route_table.data.id
+}
