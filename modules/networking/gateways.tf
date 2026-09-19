f330cd47f7a6bfad78d6ce240175d6b82919c8d8
Mahmoud <mahmoudnasser1561@gmail.com>
2026-09-19 16:15:52 +0300

    Add internet gateway and NAT gateway

---
+++ modules/networking/gateways.tf
@@ -0,0 +1,28 @@
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.project}-igw"
+  }
+}
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = {
+    Name = "${var.project}-nat"
+  }
+
+  depends_on = [aws_internet_gateway.main]
+}
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = {
+    Name = "${var.project}-nat"
+  }
+
+  depends_on = [aws_internet_gateway.main]
+}
